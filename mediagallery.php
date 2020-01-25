fb62b716a3056e5dde514d6b2ba12a92e32a7111
dulov <du1ov@yandex.ru>
2020-01-25 22:26:07 +0300

    refactor URL in short-code

--- mediagallery.php
+++ mediagallery.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Joomla.Plugin
  * @subpackage  ContentMediaGallery
@@ -8,7 +9,17 @@
  */
 
  defined('_JEXEC') or die;
+
  include('/libs/simple_html_dom.php');
+
+ $doc = JFactory::getDocument();
+ $baseUrl = JUri::base();
+
+ $doc->addScript($baseUrl.'/plugins/content/mediagallery/libs/jquery-3.4.1.min.js');
+ $doc->addStyleSheet($baseUrl.'/plugins/content/mediagallery/libs/mediagallery.css');
+ $doc->addScript($baseUrl.'/plugins/content/mediagallery/libs/mediagallery.js');
+
+ 
 /**
  * Plug-in to enable loading modules into content (e.g. articles)
  * This uses the {mediagallery url="URL"} syntax
@@ -17,13 +28,15 @@
  */
 
 class PlgContentMediaGallery extends JPlugin {
+
     /**
      * Affects constructor behavior. If true, language files will be loaded automatically.
      *
      * @var    boolean
      * @since  3.1
      */
-    protected $autoloadLanguage = true;
+
+  protected $autoloadLanguage = true;
     
     /**
      * Shorcode regex
@@ -31,25 +44,13 @@ class PlgContentMediaGallery extends JPlugin {
      * @var    string
      */
 
-    private $regex = "/{mediagallery \s*(.*?)}/si";
+  private $regex = "/{mediagallery \s*(.*?)}/si";
     
-    public function __construct(&$subject, $config = array()) {
-      parent::__construct($subject, $config);
-    }
+  public function __construct(&$subject, $config = array()) {
 
-    /**
-     * 
-     * Get params 
-     * 
-     */
+  parent::__construct($subject, $config);
 
-    function getUrl($url = '', $key = '', $value = '') {
-   
-      if (preg_match("/". $key . "\s*=\s*(\"(.*?)\")|('(.*?)')/si", $url, $matches)) {
-        $value = $matches[2];
-      }
-        return $value;
-    } 
+  }
   
     /**
      * Plugin that show hide text within content
@@ -64,134 +65,113 @@ class PlgContentMediaGallery extends JPlugin {
      * @since   1.6
      */
     
-    public function onContentPrepare($context, &$article, &$params, $page = 0) {
+  public function onContentPrepare($context, &$article, &$params, $page = 0) {
 
-      preg_match_all($this->regex, $article->text, $matches, PREG_SET_ORDER);
+    preg_match_all($this->regex, $article->text, $matches, PREG_SET_ORDER);
         
-        if ($matches) {
-            $this->parsPageToArticle($matches, $article);
-            return true;
-        } else {
-          $article->text = preg_replace($this->regex, '', $article->text, 1);
-          return true;
-          }
-    }
+      if ($matches) {
+
+        $this->parsPageToArticle($matches, $article);
+        return true;
+
+      } else {
+
+        $article->text = preg_replace($this->regex, '', $article->text, 1);
+        return true;
+
+      }
+}
+
+     /**
+     * 
+     * Get params 
+     * 
+     */
     
-    private function parsPageToArticle($matches, $article) {   
+  private function parsPageToArticle($matches, $article) {   
 
-      foreach( $matches as $match ) {
-         $url = $this->getUrl($match[0], "url"); // получаем url
-          $appendHtml = '';
-          if ($url != '') {
-            $html = file_get_html( $url ); // создание объекта парсера и получение HTML   
-            $appendHtml = $appendHtml . '<h4>'. urldecode($url) . '</h4>' . '<div class="top-content">';
-              foreach ( $html -> find ('a') as $element ) { // находим все ссылки    
-                 $filename = urldecode($element -> href );
-                  if ( preg_match('/^.*\.(jpg|jpeg|png|gif|bmp)$/i', $filename) ) {
-                    $appendHtml = $appendHtml . '<div class="div_content"><img class="image" src="' . urldecode($url) . $filename . '"' . '></div>'; 
-                  } else {
+    foreach( $matches as $match ) {
+
+      $url = $this->getUrl($match[0], "url"); // получаем url
+
+      $url = $this->parseUrl($url);
+
+      $appendHtml = '';
+
+      if ($url != '') {
+
+        $html = file_get_html( $url ); // создание объекта парсера и получение HTML   
+
+        $appendHtml = $appendHtml . '<h4>'. urldecode($url) . '</h4>' . '<div class="mediagallery__container">';
+
+        foreach ( $html -> find ('a') as $element ) { // находим все ссылки 
+
+          $filename = urldecode( $element->href );
+
+            if ( preg_match('/^.*\.(jpg|jpeg|png|gif|bmp)$/i', $filename) ) {
+
+              $appendHtml = $appendHtml . 
+              '<div class="mediagallery__item"><img class="mediagallery__image" src="' .
+              urldecode($url) .
+              $filename . 
+              '"' . 
+              '></div>'; 
+
+            } else {
                     
-                        if (preg_match('/^.*\.(mp4|webm|ogg|avi|mpeg|mpg|mkv|mov|webm|wmv)$/i', $filename)) {
-                          $path_info = pathinfo($filename);
-                          $expansion = $path_info['extension'];
-                          $appendHtml = $appendHtml . '<div class="div_content"><video controls="controls">' . '<source type="video/' . $expansion .'"'. ' src="' . urldecode($url) . $filename . '"></video></div>';
-                        }                
-                    }
-                } 
+              if ( preg_match('/^.*\.(mp4|webm|ogg|avi|mpeg|mpg|mkv|mov|webm|wmv)$/i', $filename) ) {
+
+                $path_info = pathinfo($filename);
+                $expansion = $path_info['extension'];
+                $appendHtml = $appendHtml .
+                '<div class="mediagallery__item"><video class="mediagallery__video" controls controlsList="nodownload">' .
+                '<source type="video/' .
+                $expansion . 
+                '"' . 
+                ' src="' . 
+                urldecode($url) . 
+                $filename . 
+                '"></video></div>';
+
+              }                
             }
-            $appendHtml = $appendHtml . '</div>';
-          $article->text = preg_replace($this->regex, $appendHtml, $article->text, 1);         
-        }        
+        } 
+      }
+      
+      $appendHtml = $appendHtml . '</div>';
+      $article->text = preg_replace($this->regex, $appendHtml, $article->text, 1);         
+    }        
+  }
+
+
+  function parseUrl($url) {
+
+    $url = parse_url($url);
+
+    $arr = explode('/', $url['path']);
+
+    $coded = array_map('rawurlencode', $arr);
+
+    $restored = 'http://' . $url['host'] . implode('/', $coded);
+
+    $url = $restored;
+
+    return $url; 
+
+  }
+
+  function getUrl($url = '', $key = '', $value = '') {
+
+    if (preg_match("/". $key . "\s*=\s*(\"(.*?)\")|('(.*?)')/si", $url, $matches)) {
+
+     $value = $matches[2];
+
     }
-}  
+
+     return $value;
+
+  }
+
+}
+
 ?>
-
-<style type="text/css">
-  .top-content {
-      text-align: center;
-  }
-
-  .div_content {
-      display: inline-block;
-      margin: 0px 0px 5px 5px;
-      min-height: 200px;
-      width: 300px;
-      text-align: center;
-  }
-
-  .div_content video {
-      border-radius: 10px;
-      width: 300px;
-      height: 100%;
-  }
-
-  .div_content img {
-      border-radius: 10px;
-      width: 300px;
-      height: 200px;
-  }
-
-  .image {
-      cursor: pointer;
-  }
-
-  .popup {
-      position: absolute;
-      height: 100%;
-      width: 100%;
-      top: 0;
-      left: 0;
-      display: none;
- 
-  }
-
-  .popup_bg {
-      background: rgba(0, 0, 0, 0.4);
-      position: fixed;
-      z-index: 1;
-      height: 100%;
-      width: 100%;
-  }
-
-
-  .popup_img {
-      position: fixed;
-      z-index: 2;
-      max-height: 100%;
-      max-width: 100%;
-      margin: 5% 0% 0% 0%;
-  }
-
-  .popup_img img {
-      text-align: center;
-  }
-</style>
-
-<script type="text/javascript">
-$(document).ready(function() { // Ждём загрузки страницы
-
-    $(".image").click(function() { // Событие клика на маленькое изображение
-        var img = $(this); // Получаем изображение, на которое кликнули
-        var src = img.attr('src'); // Достаем из этого изображения путь до картинки
-        $("body").append("<div class='popup'>" + //Добавляем в тело документа разметку всплывающего окна
-            "<div class='popup_bg'></div>" + // Блок, который будет служить фоном затемненным
-            "<img src='" + src + "' class='popup_img' />" + // Само увеличенное фото
-            "</div>");
-        $(".popup").fadeIn(800); // Медленно выводим изображение
-        $(".popup_bg").click(function() { // Событие клика на затемненный фон    
-            $(".popup").fadeOut(800); // Медленно убираем всплывающее окно
-            setTimeout(function() { // Выставляем таймер
-                $(".popup").remove(); // Удаляем разметку всплывающего окна
-            }, 800);
-        });
-
-        $(".popup_img").click(function() { // Событие клика на картинку   
-            $(".popup").fadeOut(800); // Медленно убираем всплывающее окно
-            setTimeout(function() { // Выставляем таймер
-                $(".popup").remove(); // Удаляем разметку всплывающего окна
-            }, 800);
-        });
-    });
-
-});
-</script>
